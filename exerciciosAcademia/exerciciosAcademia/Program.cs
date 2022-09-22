587338a0a849d8591a9d02f96c2949b2fb62c9df
Kauan Silva <kauan.silva@ufn.edu.br>
2022-09-22 17:19:02 -0300

    subindo

---
+++ exerciciosAcademia/exerciciosAcademia/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exerciciosAcademia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
+
+            float n1, n2;
+
+            Console.WriteLine("Digite um numero: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um segundo numero: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("A média aritmetica é de:" + ((n1 + n2) / 2));
+            Console.ReadKey();
+
+        }
+    }
+}
